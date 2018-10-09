5b225433c69f2e6f3c95731c1c9d3b525935a8f6
Andronikus <tiagoafroque@gmail.com>
2018-10-09 23:22:59 +0100

    Implement Map based Services. Closes #6

--- pet-clinic-data/src/main/java/pt/andronikus/sfgpetclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/pt/andronikus/sfgpetclinic/service/map/VetServiceMap.java
@@ -1,4 +1,35 @@
 package pt.andronikus.sfgpetclinic.service.map;
 
-public class VetServiceMap {
+import pt.andronikus.sfgpetclinic.model.Vet;
+import pt.andronikus.sfgpetclinic.service.CrudService;
+import pt.andronikus.sfgpetclinic.service.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(),object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
 }
