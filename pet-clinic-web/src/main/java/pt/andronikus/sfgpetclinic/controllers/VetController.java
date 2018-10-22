e5388c688bd3639f148476876bf7a5d0a3080f4e
Andronikus <tiagoafroque@gmail.com>
2018-10-22 21:59:56 +0100

    added vets list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/pt/andronikus/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/pt/andronikus/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package pt.andronikus.sfgpetclinic.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pt.andronikus.sfgpetclinic.service.VetService;
 
 @Controller
 public class VetController {
 
+    VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
+
         return "vets/index";
     }
 }
