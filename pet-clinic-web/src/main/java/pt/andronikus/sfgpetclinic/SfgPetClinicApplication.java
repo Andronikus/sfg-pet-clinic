496bdc062537c05001a3187c83a01d05f277cb81
Andronikus <tiagoafroque@gmail.com>
2018-12-25 16:54:42 +0000

    Adding static resources. Closes #17

--- pet-clinic-web/src/main/java/pt/andronikus/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/pt/andronikus/sfgpetclinic/SfgPetClinicApplication.java
@@ -2,8 +2,10 @@ package pt.andronikus.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication()
+@ComponentScan(basePackages = {"pt"})
 public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
